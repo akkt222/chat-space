6ff693405f7695650f863c01757299b85e3544f0
akkt222 <55902529+akkt222@users.noreply.github.com>
2019-12-14 18:52:18 +0900

    イベント発火

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,2 +1,14 @@
 $(function(){
+  $(".new_message").on("submit", function(e){
+    e.preventDefault()
+    console.log("イベント薄荷");
+    $.ajax({
+      url: 取得したリクエストURL,  //同期通信でいう『パス』
+      type: 'POST',  //同期通信でいう『HTTPメソッド』
+      data: 取得したFormData,  
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+  })
 });
