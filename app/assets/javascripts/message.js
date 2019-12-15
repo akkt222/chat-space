5c18cbd778565f163105d2bcc24d6cdc17d0193f
akkt222 <55902529+akkt222@users.noreply.github.com>
2019-12-15 19:04:42 +0900

    不要な箇所の削除

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -45,12 +45,11 @@ $(function(){
   }
   $(".new_message").on("submit", function(e){
     e.preventDefault()
-    console.log("イベント薄荷");
     var fd = new FormData(this);
     var url = $(this).attr("action");
     $.ajax({
-      url: url,  //同期通信でいう『パス』
-      type: 'POST',  //同期通信でいう『HTTPメソッド』
+      url: url,
+      type: 'POST',
       data: fd,
       dataType: 'json',
       processData: false,
