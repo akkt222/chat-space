eb46397cbbcfbe0318449de69e73f4476a351f43
akkt222 <55902529+akkt222@users.noreply.github.com>
2019-12-15 14:35:10 +0900

    respond_to

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,12 +2,12 @@ $(function(){
   $(".new_message").on("submit", function(e){
     e.preventDefault()
     console.log("イベント薄荷");
-    var formdata = new FormData(this);
+    var fd = new FormData(this);
     var url = $(this).attr("action");
     $.ajax({
       url: url,  //同期通信でいう『パス』
       type: 'POST',  //同期通信でいう『HTTPメソッド』
-      data: FormData,
+      data: fd,
       dataType: 'json',
       processData: false,
       contentType: false
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,5 @@
 class MessagesController < ApplicationController
-  before_action :set_group
+  before_action :set_group,
 
   def index
     @message = Message.new
@@ -15,12 +15,17 @@ class MessagesController < ApplicationController
       flash.now[:alert] = 'メッセージを入力してください。'
       render :index
     end
+    respond_to do |format|
+      format.html { redirect_to group_messages_path, notice: "メッセージを送信しました" }
+      format.json
+    end
   end
 
   private
 
   def message_params
     params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+    
   end
 
   def set_group
