ed060b7b48539e8e30589d36ebe6dad6e3e3ad47
akkt222 <55902529+akkt222@users.noreply.github.com>
2019-12-22 15:23:35 +0900

    console.logをalertに変更

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -17,7 +17,7 @@ $(function(){
       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
     })
     .fail(function() {
-      console.log('error');
+      alert('error');
     });
   };
 
